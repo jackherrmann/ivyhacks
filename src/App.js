e4e8dc7df6a2b88de64d8d7e479d7ed94c26bb5a
Eric Li <ericli52034@gmail.com>
2020-10-01 19:46:00 -0400

    Sign up page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// import LandingPage from './components/Landing';
+import LandingPage from './components/Landing'
+import Navigation from './components/Navigation';
+import SignInPage from './components/SignIn';
+import SignUpPage from './components/SignUp';
+
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      </div>
+    </Router>
   );
 }
 
