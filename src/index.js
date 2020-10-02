15fc1648cc88b13e7bd39ea852149d6507945a15
xavlee <xavlee@sas.upenn.edu>
2020-10-02 11:27:14 -0400

    index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = requirie("firebase/app")
+require("firebase/auth")
+require("firebase/firestore")
+
+const admin = require('firebase-admin')
+const serviceAccount = require('choozr-1906609d9dfa.json')
+
+OfflineAudioCompletionEvent.initializeApp({
+  credential: admiin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
