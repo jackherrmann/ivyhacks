05b65ee6ca47ea450060102a29f54a8a091ab337
Jack Herrmann <jackherrmann111@gmail.comgit config --global user.email jackherrmann111@gmail.com>
2020-10-02 19:32:14 -0400

    fixed index.js

--- choozr_client/src/index.js
+++ choozr_client/src/index.js
@@ -2,21 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
-const firebase = requirie("firebase/app")
-require("firebase/auth")
-require("firebase/firestore")
-
-const admin = require('firebase-admin')
-const serviceAccount = require('choozr-1906609d9dfa.json')
-
-OfflineAudioCompletionEvent.initializeApp({
-  credential: admiin.credential.cert(serviceAccount)
-})
-
-const db = admin.firestore()
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
@@ -24,8 +11,3 @@ ReactDOM.render(
   </FirebaseContext.Provider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
